79942820e13104af8f023727b91c2f49eff13c77
wunan <wunan.megumi@bytedance.com>
2025-10-17 11:35:22 +0800

    stash

--- unit_test/example.go
+++ unit_test/example.go
@@ -1,9 +1,18 @@
 package unit_test
 
+import "github.com/nan-www/convToMap/unit_test/aa"
+
 //go:generate convToMap example.go
 type Example struct {
-	ID     int               `json:"id,omitempty"`
-	Name   string            `json:"name,omitempty"`
-	Float  float64           `json:"float64,omitempty"`
-	Ignore map[string]string `json:"-"`
+	ID           int               `json:"id,omitempty"`
+	Name         string            `json:"name,omitempty"`
+	Float        float64           `json:"float64,omitempty"`
+	Ignore       map[string]string `json:"-"`
+	InlineStruct `json:",inline"`
+	aa.Hamabe    `json:",inline"`
+}
+
+type InlineStruct struct {
+	A string
+	B int
 }
